b132b397ed72478ad6c8925b8c16a5445196808a
cinos81 <cinos81@gmail.com>
2025-05-11 14:18:39 +0900

    fix: 수정된 npm 플러그인 설정 및 package.json 경로 업데이트

--- release.config.ts
+++ release.config.ts
@@ -8,12 +8,12 @@ const config: GlobalConfig = {
     '@semantic-release/commit-analyzer', // 커밋 메시지를 분석하여 버전 결정
     '@semantic-release/release-notes-generator', // CHANGELOG.md에 들어갈 릴리스 노트를 생성
     '@semantic-release/changelog', // CHANGELOG.md 업데이트
-    ['@semantic-release/npm', { pkgRoot: 'sdk' }], // package.json 파일의 버전을 자동으로 갱신
+    '@semantic-release/npm', // package.json 파일의 버전을 자동으로 갱신
     '@semantic-release/github', // GitHub Release를 생성
     [
       '@semantic-release/git', //  Git 커밋 및 푸시
       {
-        assets: ['CHANGELOG.md', 'sdk/package.json'],
+        assets: ['CHANGELOG.md', 'package.json'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
