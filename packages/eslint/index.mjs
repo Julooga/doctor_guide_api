a34bf2966f6425054a588ec15a2020f969b21821
cinos81 <cinos81@gmail.com>
2025-06-03 15:38:10 +0900

    feat:husky 레시피 정리

--- packages/eslint/index.mjs
+++ packages/eslint/index.mjs
@@ -1,7 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import unusedImports from 'eslint-plugin-unused-imports'
 
@@ -188,26 +186,11 @@ const defaultCodeStyle = {
   }
 }
 
-export default tseslint.config(
-  { ignores: ['build', '.react-router', '.wrangler'] },
-  defaultCodeStyle,
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ]
-    }
+export default tseslint.config(defaultCodeStyle, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: globals.browser
   }
-)
+})
