f58d8316eb4455b9c47ccdba2f16d0acde799e77
cinos81 <cinos81@gmail.com>
2025-06-10 21:41:17 +0900

    feat: '@ai-sdk/anthropic' 패키지 추가 및 버전 업데이트

--- apps/api/src/services/medcalQA/bedrockModel.ts
+++ apps/api/src/services/medcalQA/bedrockModel.ts
@@ -7,3 +7,12 @@ const bedrock = createAmazonBedrock({
 })
 
 export default bedrock('anthropic.claude-3-5-sonnet-20240620-v1:0')
+
+// import { createAnthropic } from '@ai-sdk/anthropic'
+
+// const anthropic = createAnthropic({
+//   apiKey:
+//     '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'
+// })
+
+// export default anthropic('claude-3-haiku-20240307')
