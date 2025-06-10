b24202432c0d9c2b409c28e4d0b1d6d308275b34
cinos81 <cinos81@gmail.com>
2025-06-10 23:11:20 +0900

    feat: @ai-sdk/amazon-bedrock 패키지를 추가하고 관련 코드 구현

--- apps/api/src/services/medcalQA/bedrockModel.ts
+++ apps/api/src/services/medcalQA/bedrockModel.ts
@@ -1,18 +1,9 @@
-// import { createAmazonBedrock } from '@ai-sdk/amazon-bedrock'
-// import { fromNodeProviderChain } from '@aws-sdk/credential-providers'
+import { createAmazonBedrock } from '@ai-sdk/amazon-bedrock'
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers'
 
-// const bedrock = createAmazonBedrock({
-//   region: 'ap-northeast-2',
-//   credentialProvider: fromNodeProviderChain()
-// })
-
-// export default bedrock('anthropic.claude-3-5-sonnet-20240620-v1:0')
-
-import { createAnthropic } from '@ai-sdk/anthropic'
-
-const anthropic = createAnthropic({
-  apiKey:
-    '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'
+const bedrock = createAmazonBedrock({
+  region: 'ap-northeast-2',
+  credentialProvider: fromNodeProviderChain()
 })
 
-export default anthropic('claude-3-haiku-20240307')
+export default bedrock('anthropic.claude-3-5-sonnet-20240620-v1:0')
